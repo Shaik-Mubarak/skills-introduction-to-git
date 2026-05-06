634f17256a6e0c1309f95dde30b4d64514950b7e
Shaik Mulla Mahammad Mubarak <shaikmubarak2731@gmail.com>
2026-05-06 06:36:32 +0000

    Fix high score correctly

--- src/index.js
+++ src/index.js
@@ -309,7 +309,7 @@ function clearPattern(startRow, startCol) {
 // Update score display
 function updateScore() {
   document.getElementById("score").textContent = score;
-  
+
   // Update high score if current score exceeds it
   if (score > highScore) {
     highScore = score;
